e6e22a60c499aacff9d5fe0ebc8b8ba5c43cdfe9
sword4869 <1084662708@qq.com>
2022-11-28 10:57:30 +0800

    devices

--- Click and Position/qidian/main.py
+++ Click and Position/qidian/main.py
@@ -2,18 +2,16 @@ import subprocess
 from PIL import Image
 import time
 
-# swipe screen to flush
-
 
 def swipe_flush():
+    '''swipe screen to flush'''
     x1, y1, x2, y2, duration = 1050, 600, 1050, 1600, 500
     cmd = f"adb shell input swipe {x1} {y1} {x2} {y2} {duration}"
     subprocess.check_output(cmd, shell=True)
 
-# screen shot and upload
-
 
 def get_screen():
+    '''screen shot and upload'''
     cmds = [f'adb shell screencap -p /sdcard/1.png',
             'adb pull /sdcard/1.png',
             'adb shell rm /sdcard/1.png']
@@ -21,22 +19,21 @@ def get_screen():
         subprocess.check_output(cmd, shell=True)
     return Image.open('1.png').convert("RGB")
 
-# discriminator status
-
 
 def discriminator(pos: tuple, img: Image, color: list):
+    '''discriminator status'''
     color_now = img.getpixel(pos)
     print(color_now)
-    return color != color_now 
-
-# submit a ticket
+    return color != color_now
 
 
 def click_bonus(pos: tuple):
+    '''submit a ticket'''
     cmd = f'adb shell input tap {pos[0]} {pos[1]}'
     for i in range(3):
         subprocess.check_output(cmd, shell=True)
         time.sleep(0.3)
+
 
 def close_failure(pos: tuple):
     cmd = f'adb shell input tap {pos[0]} {pos[1]}'
@@ -53,45 +50,65 @@ def train():
         img.show()
         click_bonus(pos_submit)
         close_failure(pos_other)
-        break
 
 
-def test(pos: tuple):
-    def paint_pos(pos: tuple, img: Image):
-        pos_x, pos_y = pos[0], pos[1]
-        for i in range(pos_x - 10, pos_x + 10):
-            for j in range(pos_y - 10, pos_y + 10):
-                img.putpixel((i, j), (0, 255, 0))
-
-    img = Image.open('1.png').convert("RGB")
-    color_now = img.getpixel(pos)
-    print(color_now)
-    paint_pos(pos, img)
-    img.show()
+def test():
+    import numpy as np
+    import cv2 as cv
+    # mouse callback function
+    def draw_circle(event,x,y,flags,param):
+        if event == cv.EVENT_LBUTTONDBLCLK:
+            cv.circle(img,(x,y),50,(255,0,0),-1)
+            print(x,y)
+            
+    img = cv.imread('1.png')
+    # height, width
+    print(img.shape)
+    cv.namedWindow('image', cv.WINDOW_GUI_NORMAL)
+    cv.setMouseCallback('image',draw_circle)
+    while(1):
+        cv.imshow('image',img)
+        if cv.waitKey(20) & 0xFF == 27:
+            break
+    cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    # # 有红包记录时, 已抢完状态的颜色
-    # color_history = (224, 224, 224)
-    # # 因为今日没有红包记录, 所以是背景色
-    # color_blank = (245, 245, 245)
-    # # 刷新中的背景色
-    # color_blank2 = (255, 255, 255)
+    import configargparse
+    parser = configargparse.ArgumentParser()
+    parser.add_argument('--test', action='store_true', help='test()')
+    parser.add_argument('--device', type=str, help='v,r')
+    args = parser.parse_args()
+
+
+
+    import yaml
+
+    with open('pos.yaml') as f:
+        data = yaml.safe_load(f)
+
+    if args.device == None:
+        device_type = 'vivox9'
+        # device_type = 'realmeq3pro'
+    else:
+        if args.device == 'v':
+            device_type = 'vivox9'
+        elif args.device == 'r':
+            device_type = 'realmeq3pro'
+
+    print(f'device_type = {device_type}')
+    data = data[device_type]
 
     # 红色可抢
-    color_true = (229, 53, 62)
-
-    # 热门活动
-    pos_tabbar = (540, 350)
-
+    color_true = eval(data['color_true'])
     # 红包记录的位置
-    pos_status = (833, 833)
-    # pos_status = (958, 696)
-
+    pos_status = eval(data['pos_status'])
     # 投票按钮
-    pos_submit = (533, 1400)
+    pos_submit = eval(data['pos_submit'])
+    # 空白退出
+    pos_other = eval(data['pos_other'])
 
-    pos_other = (900, 1700)
-
-    train()
-    # test(pos_status)
+    if args.test:
+        test()
+    else:
+        train()
